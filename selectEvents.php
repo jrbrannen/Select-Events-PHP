81fde29c9d3221c4f28e3216e710a6f9fb18b273
jrbrannen <71732832+jrbrannen@users.noreply.github.com>
2021-10-08 13:09:22 -0400

    Update selectEvents.php
    Added more comments

--- selectEvents.php
+++ selectEvents.php
@@ -19,17 +19,15 @@ catch(PDOException $e){
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatable" content="IE=edge">
             <meta name="viewport" content="width=device-width, intial-scale=1.0">
-            <title>Document</title>
-            <!--Jeremy Brannen-->
+            <title>Select Events</title>
+
+            <!--Jeremy Brannen
+                WDV341 7-1 Select Events-->
+
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
             <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
             <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-            
-            <script>
-
-            
-            </script>
 
             <style>
                     
@@ -56,7 +54,7 @@ catch(PDOException $e){
             <h1 class="text-center"> WDV341 Intro PHP </h1>
             <h2 class="text-center"> Select Events-PHP </h2>
 
-            <table class="table">
+            <table class="table"> <!-- Displays a table to the browser with headers-->
                 <thead>
                     <tr>
                         <th scope="col">Event Id</th>
@@ -67,7 +65,7 @@ catch(PDOException $e){
                 </thead>
 
                 <?php
-                    foreach($stmt->fetchAll(PDO::FETCH_ASSOC) as $result) {
+                    foreach($stmt->fetchAll(PDO::FETCH_ASSOC) as $result) {     // php is coded so that no data or html will show if database is empty
                 ?>      
                     <tbody>
                         <tr>
